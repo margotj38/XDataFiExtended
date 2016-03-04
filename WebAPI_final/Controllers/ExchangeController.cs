f256f7afa800fa5dc70f5e21bf2090a190f5f1af
margotj38 <john-elie.margot@ensimag.grenoble-inp.fr>
2016-03-04 22:21:49 +0100

    Récupération taux de change en temps réel

--- WebAPI_final/Controllers/ExchangeController.cs
+++ WebAPI_final/Controllers/ExchangeController.cs
@@ -8,6 +8,7 @@ using WebAPI_final.Models.Data;
 using WebAPI_final.Models;
 using WebAPI_final.Models.GestionErreurs;
 using WebAPI_final.Models.Service;
+using System.Data;
 
 
 namespace WebAPI_final.Controllers
@@ -62,6 +63,24 @@ namespace WebAPI_final.Controllers
         public DataRetour GetExchange(DateTime d1, DateTime d2, String nameBasis, String namesComp, int freq)
         {
             setExchangeRate(nameBasis, Transformation(namesComp), d1, d2, freq);
+            return donnees;
+        }
+
+        [Route("exchange/realtime/{nameBasis}/{namesComp}")]
+        public DataRetour GetExchange(String nameBasis, String namesComp)
+        {
+            string url = "http://finance.yahoo.com/webservice/v1/symbols/" + nameBasis + namesComp + "=X/quote?format=json";
+            var model = JsonMapper._download_serialized_json_data<RootObject>(url);
+            Data data = new Data();
+            DataSet dataSet = new DataSet();
+            DataTable dt = new DataTable();
+            DataColumn[] dataColumns = new DataColumn[1];
+            dataColumns[0] = new DataColumn("Price", System.Type.GetType("System.String"));
+            dt.Columns.Add(dataColumns[0]);
+            dt.Rows.Add(model.list.resources[0].resource.fields.price);
+            dataSet.Tables.Add(dt);
+            data.set(dataSet, new List<string>(), new List<string>(), DateTime.Now, DateTime.Now, Data.TypeData.RealTime);
+            donnees.SetData(data);
             return donnees;
         }
 
